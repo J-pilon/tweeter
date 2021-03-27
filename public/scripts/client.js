7757bb3dc18ae5fd7dd7cf0db0253743d8681bd8
Josiah <josiah.pilon@yahoo.ca>
2021-03-27 01:34:00 +0000

    Loads tweets from the server
    Makes an ajax get request to the route /tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,62 +7,53 @@
 // Test / driver code (temporary). Eventually will get this from the server.
 
 $(document).ready(function() {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];  
-  
-  $("#form-tweet").submit(function(event) {
-    event.preventDefault();
-    const userSubmit = $("form").serialize();
-    console.log(userSubmit);
-    $.ajax("/tweets", { method: "POST", data: userSubmit })
-  });
 
-  const createTweetElement = function(tweet) {
-    let $tweet =
-    `<section id="tweets-container">
-        <header class="article-header">
-          <img ${tweet.user.avatars}>
-          <div class="tweeters-icon-name"><div>${tweet.user.name}</div><div>${tweet.user.handle}</div>
-        </div>
-        </header>
-        <footer class="article-footer">
-          <label class="feed-message" for="feed-posts"></label>
-          <div class="feed-message">${tweet.content.text}</div>
-          <div class="article-footer">      
-            <div id="day-counter">days</div>
-            <div><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></div>         
-          </div>
-        </footer> 
-    </section>`;
-    return $tweet;
-  };
-  const renderTweets = function(tweets) {
-    for (const tweet of tweets) {
-      const $tweet = createTweetElement(tweet);
-      $("#tweets-container").append($tweet);
-    }
-  };
-  return renderTweets(data);
+  $("#form-tweet").submit(onSubmit);
+  loadTweets();
+  
 });
+
+const onSubmit = function(event) {
+  event.preventDefault();
+  const userSubmit = $(this).serialize();
+  $.ajax("/tweets", { method: "POST", data: userSubmit })
+    .then(function() {
+      loadTweets();
+    })
+    .catch(function(err) {console.log(err)})
+};
+
+const loadTweets = function() {
+  $.ajax("/tweets", { method: "GET", dataType: "json" })
+    .then(function(res) {renderTweets(res)})
+    .catch(function(err) {console.log(err)});
+};
+
+const createTweetElement = function(tweet) {
+  let $tweet =
+  `<section id="tweets-container">
+      <header class="article-header">
+        <img ${tweet.user.avatars}>
+        <div class="tweeters-icon-name"><div>${tweet.user.name}</div><div>${tweet.user.handle}</div>
+      </div>
+      </header>
+      <footer class="article-footer">
+        <label class="feed-message" for="feed-posts"></label>
+        <div class="feed-message">${tweet.content.text}</div>
+        <div class="article-footer">      
+          <div id="day-counter">days</div>
+          <div><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></div>         
+        </div>
+      </footer> 
+  </section>`;
+  return $tweet;
+};
+
+const renderTweets = function(tweets) {
+  const container = $("#tweets-container")
+  container.empty();
+  for (const tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    container.prepend($tweet);
+  }
+};
